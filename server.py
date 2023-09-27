0a10a6de4d2d88a7e5fd903694c1b000c8adfb41
NimaShariatz <shariatz@ualberta.ca>
2023-09-26 21:53:31 -0600

    worked on server.py, unfinished

--- server.py
+++ server.py
@@ -30,9 +30,37 @@ import socketserver
 class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
+        print("***********NEW TEST*************")
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
+        print ("Got a request of: %s\n" % self.data) #b'GET / HTTP/1.1\r\nHost: 127.0.0.1:8080\r\nUser-Agent: curl/8.0.1\r\nAccept: */*'
         self.request.sendall(bytearray("OK",'utf-8'))
+        
+        dataInput = self.data.decode('utf-8')
+
+        requested_command = dataInput.split('\r\n')
+        print("requested_command: ", requested_command)#['GET / HTTP/1.1', 'Host: 127.0.0.1:8080', 'User-Agent: curl/8.0.1', 'Accept: */*']
+        requested_command = requested_command[0]
+        print("requested_command: ", requested_command)#GET / HTTP/1.1
+        
+        
+        command_type = requested_command.split(' ')
+        print("command_type: ", command_type)#['GET', '/', 'HTTP/1.1']
+        command_type = command_type[0] 
+        print("command_type: ", command_type)#GET
+
+        
+        requested_val = requested_command.split(' ')
+        print("requested_command: ", requested_val)# ['GET', '/', 'HTTP/1.1']
+        requested_val=requested_val[1]
+        print("requested_command: ", requested_val) # /
+        
+        print()
+        
+        
+        #so what next?
+        
+        
+        
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
